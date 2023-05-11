fdc83147b48d6c46cf6cedf41f9d0be56c4dbb99
nishtha-tiwari <129168988+nishtha-tiwari@users.noreply.github.com>
2023-05-11 16:25:20 +0530

    Add files via upload

---
+++ Gsap project/script.js
@@ -0,0 +1,48 @@
+/*var tl = gsap.timeline()
+    
+tl.from(".main h4",{
+   y:-50,
+   duration:0.8,
+   delay:0.3,
+   stagger:0.3,
+   opacity:0
+})
+.form(".circle",{
+   scale:0,
+   duration:0.8,
+   opacity:0,
+   rotateX:360
+})
+.from(".text",{
+    opacity:0,
+    duration:1.4
+})
+.from(".man",{
+    opacity:0,
+    y:200,
+    duration:0.9
+})*/
+var tl = gsap.timeline()
+
+tl.from(".main h4",{
+    y:-50,
+    duration:0.8,
+    delay:0.3,
+    stagger:0.3,
+    opacity:0
+})
+.from(".circle",{
+    scale:0,
+    duration:0.8,
+    opacity:0,
+    rotateX:360
+})
+.from(".text",{
+    opacity:0,
+    duration:1.4
+})
+.from(".man",{
+    opacity:0,
+    y:200,
+    duration:0.9
+})
